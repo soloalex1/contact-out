0986e2eedb309c2c39b130ce4d9315e4d397c9ff
Alexandre Machado <amch.smd@gmail.com>
2019-06-24 22:04:02 -0300

    WIP: Carregamento e atualização da view principal

--- app/src/main/java/com/example/contactout/MainActivity.java
+++ app/src/main/java/com/example/contactout/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.contactout;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.contactout.model.Contato;
 import com.google.firebase.auth.FirebaseUser;
@@ -106,8 +112,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 int numCol = c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
                 String contactNumber = c.getString(numCol);
 
-                // TODO finalizar implementação do processamento de resultados
+                int aux = 0;
+                boolean exists = false;
+
+                for(Contato contact : listaContatos){
+                    if(contact.getNome().equals(contactName)){
+                        exists = true;
+                        return;
+                    }
+                }
+
+                if(exists == false){
+                    for(int j = 0; j < 5; j++){
+                        if(listaContatos[j].getNome().equals("")){
+                            listaContatos[j].setNome(contactName);
+                            listaContatos[j].setNumero(Long.valueOf(contactNumber));
+                            aux = j;
+                            break;
+                        }
+                    }
+                }
             }
+
+//            saveData();
+//            reload();
+//            updateUI();
+
+            if(resCode == RESULT_CANCELED) Toast.makeText(this, "Falha ao obter contatos.", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void saveData(int i, String name, long number){
+        String nameKey = "nome_" + i;
+        String numberKey = "number_" + i;
+
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor e = sp.edit();
+        e.putString(nameKey, name);
+        e.putLong(numberKey, number);
+        e.apply();
+
+    }
+
+    // recarrega dados dos contatos
+    private void reload() {
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("prefs", MODE_PRIVATE);
+        for (int x = 0; x < 5; x++){
+            String nameKey = "nome_" + x;
+            String numberKey = "number_" + x;
+            listaContatos[x] = new Contato(sp.getString(nameKey, ""), sp.getLong(numberKey, 0));
+        }
+    }
+
+
+    // método para abrir um contato
+    private void getContact(){
+        Intent i = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        i.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(i, ADD_CONTACT);
+    }
+
+    // método para abrir o telefone
+    private void openPhone(){
+        Intent i = new Intent(Intent.ACTION_DIAL);
+        startActivity(i);
+    }
+
+    // chama o contato a partir do índice passado
+    private void callContact(int index){
+        Intent i = new Intent(Intent.ACTION_DIAL);
+        i.setData(Uri.parse("tel:" + listaContatos[index].getNumero()));
+
+        // verificando permissões de acesso do aplicativo
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+        } else startActivity(i);
     }
 }
